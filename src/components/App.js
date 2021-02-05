5d50648c90433b341051c68f75683662c6590078
ArpitaDeb <arpitadeb1990@gmail.com>
2021-02-05 17:34:09 -0500

    props to videos conversion

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,20 @@ class App extends React.Component {
   state = { videos: [] }
 
   onTermSubmit = async term => {
-    console.log(term);
+    //console.log(term);
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
     this.setState({ videos: response.data.items });
-    console.log(response.data.items);
+    //console.log(response.data.items);
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        {this.state.videos.length}
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
