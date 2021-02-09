447c608b6c83c8293f9b69631dcd6e72265ecb40
ArpitaDeb <arpitadeb1990@gmail.com>
2021-02-08 21:28:44 -0500

    custom hook refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  useEffect(() => {
-    onTermSubmit('dance');
-  }, []);
-  const onTermSubmit = async term => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    });
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+  const [videos, search] = useVideos('sunrise');
 
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+
+  //setSelectedVideo(response.data.items[0]);
   return (
     <div className="ui container">
       <SearchBar
-        onFormSubmit={onTermSubmit}
+        onFormSubmit={search}
       />
       <div className="ui grid">
         <div className="ui row">
